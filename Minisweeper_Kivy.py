f5b2bbb41a1bff392ff18523cd69b1e70401beab
ArthurDubovik <arthur.dubovik@bk.ru>
2022-07-25 13:39:34 +0300

    Interface update, added difficulty level selection

--- Minisweeper_Kivy.py
+++ Minisweeper_Kivy.py
@@ -10,7 +10,6 @@ from kivy.clock import Clock
 from kivy.factory import Factory
 from kivy.core.window import Window
 
-
 class LongpressButton(Factory.Button):
     __events__ = ('on_long_press', )
     long_press_time = Factory.NumericProperty(0.3)
@@ -26,19 +25,66 @@ class LongpressButton(Factory.Button):
         pass
     def on_release(self, *largs):
         pass
-    
+
+class SoundButton(Button):
+    def on_state(self, instance, value):
+        if value == 'normal':
+            GameScreen.sound_click.play()
+
 
 class MenuScreen(Screen):
     def __init__(self, **kw):
         super(MenuScreen, self).__init__(**kw)
-        
-        box = BoxLayout(orientation='vertical', size=(Window.width, Window.height), padding=[0,400], spacing=40) 
-        box.add_widget(Button(text='New game', font_size = 70, on_release=lambda x: set_screen('game_field'), size_hint=(.9, .05), pos_hint = {'center_x': 0.5, 'center_y': 0.6} ))
-        box.add_widget(Button(text='Exit', font_size = 70, on_release=lambda x: MineSweeperApp().stop(), size_hint=(.9, .05), pos_hint = {'center_x': 0.5, 'center_y': 0.4}))
+        box = BoxLayout(orientation='vertical', size=(Window.width, Window.height), padding=[0,450], spacing=30) 
+        new_game = SoundButton(text='New game', font_size = 70, on_release=lambda x: set_screen('game_field'), size_hint=(.9, .01), pos_hint = {'center_x': 0.5}, background_color = '#005454' )
+        box.add_widget(new_game)
+        settings = SoundButton(text='Settings', font_size = 70, on_release=lambda x: set_screen('settings'), size_hint=(.9, .01), pos_hint = {'center_x': 0.5}, background_color = '#005454' )
+        box.add_widget(settings)
+        exit = SoundButton(text='Exit', font_size = 70, on_release=lambda x: MineSweeperApp().stop(), size_hint=(.9, .01), pos_hint = {'center_x': 0.5}, background_color = '#005454')
+        box.add_widget(exit)
         self.add_widget(box)
 
+class SettingsScreen(Screen):
+    def __init__(self, **kw):
+        super(SettingsScreen, self).__init__(**kw)
+
+        box = BoxLayout(orientation='vertical', size=(Window.width, Window.height), padding=[0,400], spacing=40) 
+        self.level_1 = SoundButton(text='5 x 11, 5 mines', on_release=self.change_diff_1, font_size = 70, size_hint=(.9, .05), pos_hint = {'center_x': 0.5}, background_color = '#005454' )
+        box.add_widget(self.level_1)
+        self.level_2 = SoundButton(text='6 x 13, 10 mines', on_release=self.change_diff_2, state = 'down', font_size = 70, size_hint=(.9, .05), pos_hint = {'center_x': 0.5}, background_color = '#005454' )
+        box.add_widget(self.level_2)
+        self.level_3 = SoundButton(text='8 x 17, 15 mines', on_release=self.change_diff_3, font_size = 70, size_hint=(.9, .05), pos_hint = {'center_x': 0.5}, background_color = '#005454')
+        box.add_widget(self.level_3)
+        box.add_widget(SoundButton(text='Back', on_press=lambda x: set_screen('menu'), font_size = 70, size_hint=(.9, .05), pos_hint = {'center_x': 0.5}, background_color = '#005454'))
+        self.add_widget(box)
+
+    def change_diff_1(self, sender, height=11, width=5, mines=5):
+        self.level_1.state = 'down'
+        self.level_2.state = 'normal'
+        self.level_3.state = 'normal'
+        pole_game.pole = []
+        pole_game.init(height, width, mines)
+
+    def change_diff_2(self, sender, height=13, width=6, mines=10):
+        self.level_1.state = 'normal'
+        self.level_2.state = 'down'
+        self.level_3.state = 'normal'
+        pole_game.pole = []
+        pole_game.init(height, width, mines)
+    
+    def change_diff_3(self, sender, height=17, width=8, mines=15):
+        self.level_1.state = 'normal'
+        self.level_2.state = 'normal'
+        self.level_3.state = 'down'
+        pole_game.pole = []
+        pole_game.init(height, width, mines)
 
 class GameScreen(Screen):
+    sound_click = SoundLoader.load('./sound/click.wav')
+    sound_mine_press = SoundLoader.load('./sound/mine_press.wav')
+    sound_mine_check = SoundLoader.load('./sound/mine_check.wav')
+    sound_win = SoundLoader.load('./sound/sound_win.wav')
+    
     def __init__(self, **kw):
         super(GameScreen, self).__init__(**kw)
         Window.bind(on_keyboard=self.onBackBtn)
@@ -46,30 +92,28 @@ class GameScreen(Screen):
     def onBackBtn(self, window, key, *args):
         if key == 27:
             set_screen('menu')
-            pole_game.pole = []
-            pole_game.init(13, 6, 6)
             return True
         return False
 
     def on_enter(self):
-  
-        self.sound_click = SoundLoader.load('./sound/click.wav')
-        self.sound_mine_press = SoundLoader.load('./sound/mine_press.wav')
-        self.sound_mine_check = SoundLoader.load('./sound/mine_check.wav')
-        self.sound_win = SoundLoader.load('./sound/sound_win.wav')
+        pole_game.pole = []
+        pole_game.init(pole_game.HEIGHT, pole_game.WIDTH, pole_game.MINES)
+        
+        
 
         #Button icons
-        #self.img_banner = Icon("./img/banner.png")
-        #self.img_mine = QIcon("./img/mine.png")
-        #self.img_false_mine = QIcon("./img/false_mine.png")
-        #self.img_explode = QIcon("./img/explode_mine.png")
+        self.img_banner = './img/banner_2.png'
+        self.img_mine = './img/mine_2.png'
+        self.img_false_mine = './img/false_mine_2.png'
+        self.img_explode = './img/explode_mine_2.png'
 
         self.CELLS = []
-        self.HEIGHT = 13
-        self.WIDTH = 6
+        self.HEIGHT = pole_game.HEIGHT
+        self.WIDTH = pole_game.WIDTH
         #List of coordinates with cells of correctly marked mines
         self.CHECKED_MINES = []
-        self.MINES = 6
+        self.MINES = pole_game.MINES
+
 
         #List with cells that have been unchecked
         self.non_check_mines = []
@@ -77,22 +121,22 @@ class GameScreen(Screen):
         self.open_cells = []
 
         #Button sizes
-        but_size_x = 60
-        but_size_y = 60
-
-        #field_x = (but_size_x) * 6 + (pad_x) * 5
-        #field_y = (but_size_y + (pad_y)*2) * 13
+        if self.WIDTH < 6:
+            but_size_x = but_size_y = 180
+        if self.WIDTH == 6:
+            but_size_x = but_size_y = 160
+        if self.WIDTH > 6:
+            but_size_x = but_size_y = 120
         
         #Setting the padding
         space = 10
         pad_x = Window.width - (but_size_x * self.WIDTH + space * (self.WIDTH - 1))
         pad_y = Window.height - (but_size_y * self.HEIGHT + space * (self.HEIGHT - 1))
         
-        self.layout = GridLayout(cols=6, rows=13, padding=[pad_x/2, pad_y/2], spacing=[space])
+        self.layout = GridLayout(cols=self.WIDTH, rows=self.HEIGHT, padding=[pad_x/2, pad_y/2], spacing=[space])
         
         for i in range(self.HEIGHT):
             for j in range(self.WIDTH):
-                #size_hint_min = [but_size_x,but_size_y], size_hint_max = [but_size_x,but_size_y]
                 self.button = LongpressButton()
                 self.button.bind(on_release=self.open_cell)
                 self.button.bind(on_long_press=self.mine_check)
@@ -130,9 +174,8 @@ class GameScreen(Screen):
     def open_cell(self, sender):
         #The object of the pressed button
         if sender.mine_note != True and sender.is_pressed == False and sender.long == False:
-            #self.sound_click.stop()
             self.sound_click.play()
-            #time.sleep(0.005)
+
             sender.is_pressed = True
             sender.background_color = 180, 40, 100, 0.8
             
@@ -181,8 +224,8 @@ class GameScreen(Screen):
         
         if sender.is_pressed == False and sender.win == False:
             if sender.mine_note == False:
-                sender.background_normal = './img/banner_2.png'
-                sender.background_down = './img/banner_2.png'
+                sender.background_normal = self.img_banner
+                sender.background_down = self.img_banner
                 
                 self.sound_mine_check.play()
                 sender.mine_note = True
@@ -214,13 +257,13 @@ class GameScreen(Screen):
             i.is_pressed = True
             if [i.coord_x, i.coord_y] in pole_game.rij and i.mine_note == False:
                 i.state = 'normal'
-                i.background_normal = "./img/mine_2.png"
+                i.background_normal = self.img_mine
             if i.mine_note == True and [i.coord_x, i.coord_y] not in pole_game.rij:
-                i.background_normal = "./img/false_mine_2.png"
+                i.background_normal = self.img_false_mine
 
         sender.background_color = 1, 0, 0, 1
         #sender.background_color = 180, 40, 100, 0
-        sender.background_normal = "./img/explode_mine_2.png"
+        sender.background_normal = self.img_explode
         
 
     #Opening empty cells around the selected empty one
@@ -258,23 +301,24 @@ class Cell:
 
 #Game field
 class GameField:
-    def init(self, height=13, width=6, mines=6):
+    def init(self, height=13, width=6, mines=10):
         #List size
-        self.M = height
-        self.N = width
+        self.HEIGHT = height
+        self.WIDTH = width
         #Number of mines
-        self.mines = mines
+        self.MINES = mines
+        #print(self.HEIGHT, self.WIDTH, self.MINES)
 
         #Filling the list of cells with zeros
-        self.pole = [[(Cell(0, False)) for i in range(self.N)] for i in range(self.M)]
+        self.pole = [[(Cell(0, False)) for i in range(self.WIDTH)] for i in range(self.HEIGHT)]
 
         #List with mines
         self.rij = []
         
         #Filling the field with mines
         mine_count = 0
-        while mine_count < self.mines:
-            r = [random.randrange(self.M), random.randrange(self.N)]
+        while mine_count < self.MINES:
+            r = [random.randrange(self.HEIGHT), random.randrange(self.WIDTH)]
             #print(r)
             if r not in self.rij:
                 self.rij.append(r)
@@ -282,12 +326,12 @@ class GameField:
                 mine_count += 1
         
         #Counting the number of mines around each cell
-        for i in range(self.M):
-            for j in range(self.N):
+        for i in range(self.HEIGHT):
+            for j in range(self.WIDTH):
                 around_mines = 0
                 for x in range(-1, 2):
                     for y in range(-1, 2):
-                        if (i + x) >= 0 and (j + y) >= 0 and (i + x) < self.M and (j + y) < self.N:
+                        if (i + x) >= 0 and (j + y) >= 0 and (i + x) < self.HEIGHT and (j + y) < self.WIDTH:
                             if self.pole[i + x][j + y].mine == True:
                                 around_mines += 1
                 if self.pole[i][j].mine == True and around_mines > 0:
@@ -301,6 +345,7 @@ def set_screen(name_screen):
 
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(SettingsScreen(name='settings'))
 sm.add_widget(GameScreen(name='game_field'))
 
 class MineSweeperApp(App):
